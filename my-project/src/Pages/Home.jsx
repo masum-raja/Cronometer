2634d4c897fa661350b979e4b261fe736b964249
masum-raja <razamasum@gmail.com>
2022-09-28 21:57:37 +0530

    Added day-2 Work

--- my-project/src/Pages/Home.jsx
+++ my-project/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
-import {Container} from '@chakra-ui/react'
+import {Box, Button, Container, Link} from '@chakra-ui/react'
 import Styles from "./Home.module.css"
 
 function Home(){
@@ -8,15 +8,6 @@ function Home(){
         <div>
             <Navbar/>
             <Container maxW="100%">
-                <div className={Styles.main}>
-                    <div>
-                        <img src="https://cdn1.cronometer.com/2021/landing/cronometer-hero-graphic.svg" alt="image" />
-                    </div>
-                    <div>
-
-                    </div>
-                </div>
-
             </Container>
             <Footer/>
         </div>
